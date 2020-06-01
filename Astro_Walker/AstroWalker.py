110289fee56a620e0011ca61d6e3ad539ee2c9c5
rabmaranon <57651039+rabmaranon@users.noreply.github.com>
2020-05-31 18:34:53 -0700

    Add files via upload

---
+++ Astro_Walker/AstroWalker.py
@@ -0,0 +1,152 @@
+# im porting libraries
+import os, sys, pygame, random, math
+from pygame.locals import *
+
+# import functions from other files
+from gameFunctions import *
+from gameClasses import *
+import gameVariables
+
+""" Main method to initialize and run flappy bird game """
+def main():
+    # initialize pygame & mixer
+    screen = initialize_pygame()
+
+    # set up some timers
+    clock = pygame.time.Clock()
+    pygame.time.set_timer(getNewPipe, pipesAddInterval)
+
+    # load the images, create the bird, ground, and game list
+    gamePipes = []
+
+    # this is actually the astronaut, but it's quite a hassle to rename everything
+    gameBird = Bird()
+    gameImages = load_images()
+    gameVariables.gameScore = 0
+    gameGround = Ground(gameImages['ground'])
+
+    # load the game sounds
+    jump_sound = pygame.mixer.Sound('sounds/jump.ogg')
+    score_sound = pygame.mixer.Sound('sounds/score.ogg')
+    dead_sound = pygame.mixer.Sound('sounds/dead.ogg')
+
+    # when the user clicks, the game starts -- wait for that to happen
+    while(gameVariables.waitClick == True):
+        # draw everything and wait for the user to click to start the game
+        # when the user clicks somewhere, the astronaut will walk/jump and the game will start
+        screen.blit(gameImages['space'], (0, 0))
+        draw_text(screen, "click to start", 285, 40)
+        screen.blit(gameImages['ground'], (0, gameHeight - groundHeight))
+
+        # a new astronaut is 'drawn' every time it moves
+        gameBird.redraw(screen, gameImages['astro'], gameImages['astro'])
+
+        # update the screen
+        pygame.display.update()
+
+        # check if the user pressed left click or space and start (or not) the game
+        for e in pygame.event.get():
+            if e.type == pygame.MOUSEBUTTONDOWN or (e.type == pygame.KEYDOWN and e.key == K_SPACE):
+                gameBird.steps_to_jump = 15
+                gameVariables.waitClick = False
+    jump_sound.play()
+
+    # loop until the round is over
+    while True:
+        # draw the background
+        screen.blit(gameImages['space'], (0, 0))
+
+        # get the mouse, keyboard or user events and act accordingly
+        for e in pygame.event.get():
+            # wait for user input
+            # different inputs lead to different movements or game actions
+            if e.type == getNewPipe:
+                # use global variables so game actions are consistent
+                p = PipePair(gameWidth, False)
+                gamePipes.append(p)
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                gameBird.steps_to_jump = jumpSteps
+                jump_sound.play()
+            elif e.type == pygame.KEYDOWN:
+                if e.key == K_SPACE:
+                    gameBird.steps_to_jump = jumpSteps
+                    jump_sound.play()
+                elif e.key == K_ESCAPE:
+                    exit()
+
+        # a new frame
+        clock.tick(FPS)
+
+        # update the position of the gamePipes and redrawing them; if a pipe is not visible anymore,
+        # we remove it from the list
+        for p in gamePipes:
+            p.x -= pixelsFrame
+            # pipe is out of frame, so remove that object (Even if we don't see it)
+            if p.x <= - pipeWidth:
+                gamePipes.remove(p)
+            else:
+                screen.blit(gameImages['pipe-up'], (p.x, p.toph))
+                screen.blit(gameImages['pipe-down'], (p.x, p.bottomh))
+
+        # every time there's a new frame, redraw everything in the environment (ground, astronaut, pipe)
+        gameGround.move_and_redraw(screen)
+
+        # update the astronaut position and redrawing it
+        gameBird.update_position()
+        gameBird.redraw(screen, gameImages['astro'], gameImages['astro'])
+
+        # check for any collisions between the gamePipes, bird and/or the lower and the
+        # upper part of the screen
+
+        # a collision occurs when the coordinates of the bird overlap with the pipes
+        if any(p.check_collision((gameBird.bird_x, gameBird.bird_y)) for p in gamePipes) or \
+               gameBird.bird_y < 0 or \
+               gameBird.bird_y + birdHeight > gameHeight - groundHeight:
+            dead_sound.play()
+            break
+
+        # there were no collision if we ended up here, so we are checking to see if
+        # the dog went through one half of the pipe's gameWidth; if so, we update the gameScore
+        for p in gamePipes:
+            if(gameBird.bird_x > p.x and not p.score_counted):
+                p.score_counted = True
+                # increment global gameScore
+                gameVariables.gameScore += 1
+                score_sound.play()
+
+        # draw the gameScore on the screen
+        draw_text(screen, gameVariables.gameScore, 50, 50)
+        pygame.display.update()
+
+    # make the astronaut 'fall' if the round is over
+    while(gameBird.bird_y + birdHeight < gameHeight - groundHeight):
+        # redraw the background
+        screen.blit(gameImages['space'], (0, 0))
+
+        # redraw the gamePipes in the same place as when it died
+        for p in gamePipes:
+            screen.blit(gameImages['pipe-up'], (p.x, p.toph))
+            screen.blit(gameImages['pipe-down'], (p.x, p.bottomh))
+
+        # draw the ground piece to get the rolling effect
+        gameGround.move_and_redraw(screen)
+
+        # make the astronaut fall down and rotates it
+        gameBird.redraw_dead(screen, gameImages['astro'])
+
+        # one more tick
+        clock.tick(FPS * 3)
+
+        # update the entire screen
+        pygame.display.update()
+
+    # end the game
+    if not end_the_game(screen, gameVariables.gameScore):
+        main()
+    else:
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
---
+++ Astro_Walker/README.md
@@ -0,0 +1,33 @@
+# Astro Walker 
+#### Let's all experience walking in space!
+
+A modification of Flappy Bird, built using [PyGame](https://www.pygame.org/), with additional comments and customized graphics. Built in one day!
+
+
+
+## Inspiration
+This project lessens some problems faced when you're in isolation because it is very interactive. This is a modified version of the famous Flappy Bird game but is space themed. You can play as much as 6 players if you have good internet together and be competitive as much as you can. And there is also an offline version and you can also play with 2 players using Bluetooth if internet is not good in your area. Future development will have different levels with different planets as grounds and will incorporate a "Did you know?" section after every Game Over to be more informative. All of the fun facts will be from NASA's database so it's all going to be accurate. Also there will be an option to create your own character, be an astronaut yourself. Who doesn't want to be an astronaut, right? It's everybody's childhood dream! We're also considering to develop this into a full blown app game to all lovers of space. This is a fairly simple game but with great impact. 
+
+This is my team's first ever game - we have a little  experience in CS so we want to challenge ourselves to make a game. This is the basic version of the game. Future developments will be added in the future.
+
+## Installation
+1. Install Python 3.7.x if necessary (instuctions [here](https://realpython.com/installing-python))
+
+    `$ python --version  # check version number if you're not sure`
+
+2. Install PyGame
+
+    `$ python3 -m pip install -U pygame --user`
+
+## Usage
+#### To run the game:
+    $ python AstroWalker.py
+
+#### To play the game:
+1. Press <kbd>Space</kbd> to start the game and to move the dog up or down.
+2. The objective is to keep the dog 'flying' and squeeze between the obstacles. Once the dog collides with an obstacle, he will fall, signaling the end of a game. Press any <kbd>key</kbd> to start a new round.
+3. The game can be exited at any time by pressing <kbd>Esc</kbd>.
+
+## Thanks
+
+* [Python Flappy Bird Project](https://github.com/popadi/python-flappy-bird) - All of the code presented here is based off this project.
